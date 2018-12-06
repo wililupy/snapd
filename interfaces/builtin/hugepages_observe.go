a0a1ef9b6cae748c5759fd1e5c898f03554b170c
Lucas Williams <wililupy@comcast.net>
2018-12-06 09:55:40 -0800

    Add files via upload
    Hugepages Interface GO

---
+++ interfaces/builtin/hugepages_observe.go
@@ -0,0 +1,49 @@
+// -*- Mode: Go; indent-tabs-mode: t -*-
+
+/*
+ * Copyright (C) 2018 Canonical Ltd
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License version 3 as
+ * published by the Free Software Foundation.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ *
+ */
+
+package builtin
+
+const hugepagesObserveSummary = `allows using hugepages`
+
+const hugepagesObserveBaseDeclarationSlots = `
+  hugepages-control:
+    allow-installation:
+      slot-snap-type:
+        - core
+    deny-auto-connection: true
+`
+
+const hugepagesObserveConnectedPlugAppArmor = `
+# Description: Allow control to hugepages.
+/sys/kernel/mm/hugepages/{,*} r,
+/dev/hugepages/{,**} r,
+/run/hugepages/{,**} r,
+`
+
+func init() {
+	registerIface(&commonInterface{
+		name:                     "hugepages-control",
+		summary:                  hugepagesObserveSummary,
+		implicitOnCore:           true,
+		implicitOnClassic:        true,
+		reservedForOS:            true,
+		baseDeclarationSlots:     hugepagesObserveBaseDeclarationSlots,
+		connectedPlugAppArmor:    hugepagesObserveConnectedPlugAppArmor,
+	})
+}
